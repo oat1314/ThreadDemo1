c869156d08c41a7db14eb345c00db0c7338ba259
iverson <403963470@qq.com>
2020-10-18 23:23:12 +0800

    demo

---
+++ src/main/java/Demo/t7/Run.java
@@ -0,0 +1,11 @@
+package Demo.t7;
+
+public class Run {
+    public static void main(String[] args) throws InterruptedException {
+        MyThread myThread = new MyThread();
+        System.out.println("begin=="+myThread.isAlive());
+        myThread.start();
+        Thread.sleep(1000);
+        System.out.println("end=="+myThread.isAlive());
+    }
+}
