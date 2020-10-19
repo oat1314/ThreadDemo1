1cb8aaf7f16c3aa4a4ff4b4875d738dd78306856
iverson <403963470@qq.com>
2020-10-19 23:11:34 +0800

    demo

--- src/main/java/Demo/t12/Run3.java
+++ src/main/java/Demo/t12/Run3.java
@@ -1,14 +1,14 @@
 package Demo.t12;
 
-public class Run {
+public class Run3 {
     public static void main(String[] args) {
         try {
             MyThread thread = new MyThread();
             thread.start();
             Thread.sleep(1000);
             thread.interrupt();
-            System.out.println("是否停止1 ?= "+Thread.interrupted());
-            System.out.println("是否停止2 ?= "+Thread.interrupted());
+            System.out.println("是否停止1?="+thread.isInterrupted());
+            System.out.println("是否停止2?="+thread.isInterrupted());
         } catch (Exception e) {
             System.out.println("main catch");
             e.printStackTrace();
